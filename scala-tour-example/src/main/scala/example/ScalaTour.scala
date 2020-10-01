76fd6de2294ed46addff2b3661da04bd4b138cbf
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2020-10-01 23:56:26 +0900

    Parametric polymorphism

--- scala-tour-example/src/main/scala/example/ScalaTour.scala
+++ scala-tour-example/src/main/scala/example/ScalaTour.scala
@@ -2,7 +2,40 @@ package example
 
 object ScalaTour {
   def main(args: Array[String]): Unit = {
-    val message = "Hello World"
-    println(message)
+    var intBox = new Box[Int](10)
+    println(intBox.get())
+  }
+
+  def fizzBuzz(n: Int): Unit =
+    for { i <- (1 to n) } {
+      if (i % 15 == 0) {
+        println("FizzBuzz")
+      } else if (i % 3 == 0) {
+        println("Fizz")
+      } else if (i % 5 == 0) {
+        println("Buzz")
+      } else {
+        println(i)
+      }
+    }
+  def fizzBuzz2(n: Int): Unit =
+    for { i <- (1 to n) } {
+      i match {
+        case x if x % 15 == 0 =>
+          println("FizzBuzz")
+        case x if x % 3 == 0 =>
+          println("Fizz")
+        case x if x % 5 == 0 =>
+          println("Buzz")
+        case x =>
+          println(x)
+      }
+    }
+}
+
+class Box[T](var element: T) {
+  def get(): T = element
+  def set(newElement: T): Unit = {
+    element = newElement
   }
 }
